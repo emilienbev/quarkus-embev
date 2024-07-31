81f99ccb6815a1f4654091684b7198a3072fd1fd
Emilien Bevierre <emilien.bevierre@couchbase.com>
2024-07-31 17:24:28 +0100

    substitute ResolverConfig constructor and remove Windows/Android providers.

--- runtime/src/main/java/com/couchbase/quarkus/extension/runtime/CouchbaseSubstitutions.java
+++ runtime/src/main/java/com/couchbase/quarkus/extension/runtime/CouchbaseSubstitutions.java
@@ -1,10 +1,19 @@
 package com.couchbase.quarkus.extension.runtime;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BooleanSupplier;
 
+import com.couchbase.client.core.deps.org.xbill.DNS.Name;
+import com.couchbase.client.core.deps.org.xbill.DNS.ResolverConfig;
+import com.couchbase.client.core.deps.org.xbill.DNS.SimpleResolver;
+import com.couchbase.client.core.deps.org.xbill.DNS.config.*;
 import com.couchbase.client.core.encryption.CryptoManager;
 import com.couchbase.client.java.codec.*;
 import com.couchbase.client.java.env.ClusterEnvironment;
+import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
@@ -50,6 +59,66 @@ final class TargetClusterEnvironment {
             } catch (ClassNotFoundException ignored) {
                 return true;
             }
+        }
+    }
+}
+
+@TargetClass(value = ResolverConfig.class)
+final class Target_ResolverConfig {
+    @Alias
+    private List<InetSocketAddress> servers = new ArrayList<>(2);
+    @Alias
+    private List<Name> searchlist = new ArrayList<>(0);
+    @Alias
+    private int ndots = 1;
+    @Alias
+    private static ResolverConfig currentConfig;
+    @Alias
+    private static List<ResolverConfigProvider> configProviders;
+
+    @Substitute
+    public Target_ResolverConfig() {
+        synchronized (ResolverConfig.class) {
+            if (configProviders == null) {
+                configProviders = new ArrayList<>(5);
+                configProviders.add(new PropertyResolverConfigProvider());
+                configProviders.add(new ResolvConfResolverConfigProvider());
+                configProviders.add(new JndiContextResolverConfigProvider());
+                configProviders.add(new SunJvmResolverConfigProvider());
+                configProviders.add(new FallbackPropertyResolverConfigProvider());
+
+            }
+        }
+
+        for (ResolverConfigProvider provider : configProviders) {
+            if (provider.isEnabled()) {
+                try {
+                    provider.initialize();
+                    if (servers.isEmpty()) {
+                        servers.addAll(provider.servers());
+                    }
+
+                    if (searchlist.isEmpty()) {
+                        List<Name> lsearchPaths = provider.searchPaths();
+                        if (!lsearchPaths.isEmpty()) {
+                            searchlist.addAll(lsearchPaths);
+                            ndots = provider.ndots();
+                        }
+                    }
+
+                    if (!servers.isEmpty() && !searchlist.isEmpty()) {
+                        // found both servers and search path, we're done
+                        return;
+                    }
+                } catch (InitializationException e) {
+                    //Ignore
+                }
+            }
+        }
+
+        if (servers.isEmpty()) {
+            servers.add(
+                    new InetSocketAddress(InetAddress.getLoopbackAddress(), SimpleResolver.DEFAULT_PORT));
         }
     }
 }
